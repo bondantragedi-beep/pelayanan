a1b944c977f140e7a2a529c4616fe6c8d73fff97
bondantragedi-beep <bondantragedi@gmail.com>
2026-09-21 15:27:33 +0800

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+$request->validate([
+    'login_type' => 'required|in:sekolah,verifikator',
+    'password'   => 'required',
+    'npsn'       => 'required_if:login_type,sekolah',
+    'nip'        => 'required_if:login_type,verifikator|digits:18',
+]);
 
-abstract class Controller
-{
-    //
+if ($request->login_type === 'sekolah') {
+    // Auth::guard('sekolah')->attempt(['npsn' => ..., 'password' => ...])
+} else {
+    // Auth::guard('verifikator')->attempt(['nip' => ..., 'password' => ...])
 }
