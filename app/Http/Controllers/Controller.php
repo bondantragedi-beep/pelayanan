9c1d8e8b70648a1bc65f0c98aaadd4b86eee19a1
bondantragedi-beep <bondantragedi@gmail.com>
2026-09-23 13:53:38 +0800

    Add files via upload

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,14 +1,8 @@
 <?php
 
-$request->validate([
-    'login_type' => 'required|in:sekolah,verifikator',
-    'password'   => 'required',
-    'npsn'       => 'required_if:login_type,sekolah',
-    'nip'        => 'required_if:login_type,verifikator|digits:18',
-]);
+namespace App\Http\Controllers;
 
-if ($request->login_type === 'sekolah') {
-    // Auth::guard('sekolah')->attempt(['npsn' => ..., 'password' => ...])
-} else {
-    // Auth::guard('verifikator')->attempt(['nip' => ..., 'password' => ...])
+abstract class Controller
+{
+    //
 }
