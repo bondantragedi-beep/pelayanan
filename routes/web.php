d2fb0eb4e2090331910ea40d3e3872f54136b260
bondantragedi-beep <bondantragedi@gmail.com>
2026-09-23 13:04:37 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/login', [LoginController::class, 'create'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
